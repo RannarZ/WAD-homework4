98ad854938d57ffdd20853800cca20e326448993
OskarMannik <mannikoskar@gmail.com>
2024-12-14 17:01:10 +0200

    added initial AddPost and APost pages

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,6 +2,8 @@ import { createRouter, createWebHistory } from 'vue-router';
 import MainPage from '@/views/MainPage.vue';
 import SignupPage from '@/views/SignupPage.vue';
 import ContactsPage from '@/views/ContactsPage.vue';
+import AddPostPage from '@/views/AddPostPage.vue';
+import APostPage from '@/views/APostPage.vue';
 
 
 // defining the routes array, mapping paths to components
@@ -10,6 +12,8 @@ const routes = [
   { path: '/', name: 'MainPage', component: MainPage },
   { path: '/signup', name: 'SignupPage', component: SignupPage },
   { path: '/contactus', name: 'ContactsPage', component: ContactsPage },
+  { path: '/addpost', name: 'AddPostPage', component: AddPostPage },
+  { path: '/apost', name: 'APostPage', component: APostPage },
 ];
 
 const router = createRouter({
